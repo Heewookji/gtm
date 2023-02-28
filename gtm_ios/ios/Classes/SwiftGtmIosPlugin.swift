771c5fb426697ff4d23d1960fa406be24aa79e2a
Heewookji <jhu2269@gmail.com>
2023-02-28 14:52:55 +0900

    feat: version example까지

--- gtm_ios/ios/Classes/SwiftGtmIosPlugin.swift
+++ gtm_ios/ios/Classes/SwiftGtmIosPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftGtmIosPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "gtm_ios", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "heewook.kr/gtm_ios", binaryMessenger: registrar.messenger())
     let instance = SwiftGtmIosPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
