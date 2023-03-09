87715aed570b20a7dddd2b2ec9fe9a2346a2780d
Heewookji <jhu2269@gmail.com>
2023-03-09 11:18:03 +0900

    feat: android gtm custom Tag

--- gtm_android/android/src/main/kotlin/kr/heewook/gtm_android/GtmAndroidPlugin.kt
+++ gtm_android/android/src/main/kotlin/kr/heewook/gtm_android/GtmAndroidPlugin.kt
@@ -1,35 +1,105 @@
 package kr.heewook.gtm_android
 
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
 import androidx.annotation.NonNull
-
+import com.google.android.gms.tagmanager.CustomTagProvider
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import org.json.JSONException
+import org.json.JSONObject
+import io.flutter.plugin.common.MethodChannel.Result as MethodChannelResult
+
 
 /** GtmAndroidPlugin */
-class GtmAndroidPlugin: FlutterPlugin, MethodCallHandler {
+class GtmAndroidPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  companion object {
+    @JvmStatic
+    lateinit var channel: MethodChannel
+    @JvmStatic
+    lateinit var activity: Activity
+  }
+  private lateinit var firebaseAnalytics: FirebaseAnalytics
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    firebaseAnalytics = Firebase.analytics
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "heewook.kr/gtm_android")
     channel.setMethodCallHandler(this)
   }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
   }
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    onAttachedToActivity(binding)
+  }
+  override fun onDetachedFromActivityForConfigChanges() {}
+
+  override fun onDetachedFromActivity() {}
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannelResult) {
+    try {
+      val args = decodeArguments(call.arguments as String)
+      when (call.method) {
+        "push" -> {
+          val eventName = args.getString("eventName")
+          val eventParameters = jsonToBundle(args.getJSONObject("eventParameters"))
+          firebaseAnalytics.logEvent(eventName, eventParameters)
+          result.success(true)
+        }
+        else -> result.notImplemented()
+      }
+    } catch (e: Exception) {
+      result.error("EXCEPTION_IN_HANDLE", e.message, null)
+    }
+  }
+  // helper function to decode arguments
+  @Throws(Exception::class)
+  private fun decodeArguments(arguments: String): JSONObject {
+    return JSONObject(arguments)
+  }
+
+  @Throws(JSONException::class)
+  fun jsonToBundle(jsonObject: JSONObject): Bundle? {
+    val bundle = Bundle()
+    val iterator: Iterator<*> = jsonObject.keys()
+    while (iterator.hasNext()) {
+      val key = iterator.next() as String
+      val value = jsonObject.getString(key)
+      bundle.putString(key, value)
+    }
+    return bundle
+  }
+
+}
+
+class CustomTag: CustomTagProvider {
+  @Throws(Exception::class)
+  private fun encodeArguments(argumentsMap: Map<String, Any>): String {
+    return JSONObject(argumentsMap).toString()
+  }
+  override fun execute(@NonNull parameters: Map<String, Any>) {
+    return try {
+      GtmAndroidPlugin.activity.runOnUiThread {
+        GtmAndroidPlugin.channel.invokeMethod("CustomTag", encodeArguments(parameters))
+      }
+    } catch (e: Exception) {
+      e.message?.let { Log.d("Gtm CustomTag error", it) }
+      return
+    }
   }
 }
