8584df517e0f454fb82facd7a2dc622cabfac2f6
heewookji <jhu2269@gmail.com>
2024-09-19 17:47:22 +0900

    fix: web example

--- gtm/example/lib/main.dart
+++ gtm/example/lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gtm/gtm.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (!kIsWeb) {
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- gtm_web/example/lib/main.dart
+++ gtm_web/example/lib/main.dart
@@ -1,17 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gtm_web/gtm_web.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    try {
+      final gtm = GtmWeb();
+      gtm.push(
+        'test',
+        parameters: {
+          'user_no': 912342,
+        },
+      );
+      gtm.push(
+        'readCase',
+        parameters: {
+          'user_no': 912342,
+          'user_type': 2,
+        },
+      );
+      gtm.push(
+        'buyEduCamp',
+        parameters: {
+          'user_no': 912342,
+          'price': 10000.0,
+        },
+      );
+    } on PlatformException {
+      print('exception occurred!');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
